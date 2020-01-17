1653eccc6ebbc9fa12b8a96d4b79360f5908530c
Ali Bidar <bidar1985@gmail.com>
2020-01-17 10:44:43 +0330

    Updates

---
+++ precache-manifest.5fa312fb786f9f446415969675a18c9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c58498663e38ae153c5ec929c40347",
+    "url": "/iamalibidar/index.html"
+  },
+  {
+    "revision": "c21553cc155ad873a0ab",
+    "url": "/iamalibidar/static/css/main.89247b7a.chunk.css"
+  },
+  {
+    "revision": "3e6abe65345d227f0466",
+    "url": "/iamalibidar/static/js/2.c74c42e1.chunk.js"
+  },
+  {
+    "revision": "d9bc9aff9b81d31e8e002b5479cfe228",
+    "url": "/iamalibidar/static/js/2.c74c42e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c21553cc155ad873a0ab",
+    "url": "/iamalibidar/static/js/main.c5ea4612.chunk.js"
+  },
+  {
+    "revision": "becd6039cdfd320dd5cc",
+    "url": "/iamalibidar/static/js/runtime-main.a5557b3f.js"
+  }
+]);
